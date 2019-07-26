769d7119e2b065470a23e45b608e93880d025eb0
fesanto <fgesanto@gmail.com>
2019-07-26 14:27:20 -0300

    media query 600px

--- script/script.js
+++ script/script.js
@@ -1,9 +1,3 @@
-$(document).ready(function () {
-    $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function () {
-        $(this).toggleClass('open');
-    });
-});
-
 let slideIndex = 1;
 showSlides(slideIndex);
 
